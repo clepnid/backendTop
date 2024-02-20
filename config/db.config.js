a4d0fab01b5d93c482d3bf73550f8b2a5bac18a2
clepnid <clepnid@gmail.com>
2024-02-20 18:15:35 +0100

    add deploy vercel sql

--- config/db.config.js
+++ config/db.config.js
@@ -2,14 +2,35 @@
 
 const dotenv = require('dotenv').config();
 const mysql = require('mysql');
+const fs = require('fs');
+const path = require('path');
+
+const mode = 'lcal'
+var dbConn;
+const caPath = path.join(__dirname, 'isrgrootx1.pem');
+
 
 //local mysql db connection
-const dbConn = mysql.createConnection({
-  host     : process.env.MYSQL_IP || 'localhost',
-  user     : process.env.MYSQL_USER ||  'root',
-  password : process.env.MYSQL_PASSWORD ||  '',
-  database : process.env.MYSQL_DB ||  'tfg'
-});
+if (mode === 'local') {
+  dbConn = mysql.createConnection({
+    host     : process.env.MYSQL_IP || 'localhost',
+    user     : process.env.MYSQL_USER ||  'root',
+    password : process.env.MYSQL_PASSWORD ||  '',
+    database : process.env.MYSQL_DB ||  'tfg'
+  });
+}else{
+  dbConn = mysql.createConnection({
+    host     : process.env.TIDBCLOUD_MYSQL_IP || 'localhost',
+    port     : process.env.TIDBCLOUD_MYSQL_PORT ||  '',
+    user     : process.env.TIDBCLOUD_MYSQL_USER ||  'root',
+    password : process.env.TIDBCLOUD_MYSQL_PASSWORD ||  '',
+    database : process.env.TIDBCLOUD_MYSQL_DB ||  'tfg',
+    ssl: {
+         ca: fs.readFileSync(caPath)
+     }
+  });
+}
+
 
 dbConn.connect(function(err) {
   if (err) throw err;
